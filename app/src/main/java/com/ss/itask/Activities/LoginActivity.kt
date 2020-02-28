a2d56f69a58d1fdd01e3830a31c433ffe564d215
Soilih BEN SOILIH <s.bensoilih@gmail.com>
2020-02-28 12:19:47 -0500

    register et login

---
+++ app/src/main/java/com/ss/itask/Activities/LoginActivity.kt
@@ -0,0 +1,36 @@
+package com.ss.itask.Activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.ss.itask.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val editText_email = findViewById<EditText>(R.id.editText_email)
+        val editText_password = findViewById<EditText>(R.id.editText_password)
+        val button_login = findViewById<Button>(R.id.button_login)
+        val textView_register = findViewById<TextView>(R.id.textView_register)
+        val textView_skip = findViewById<TextView>(R.id.textView_skip)
+
+
+        textView_register.setOnClickListener(){
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        textView_skip.setOnClickListener(){
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/ss/itask/Activities/RegisterActivity.kt
@@ -0,0 +1,25 @@
+package com.ss.itask.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.ss.itask.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val editText_pseudo = findViewById<EditText>(R.id.editText_pseudo)
+        val editText_email = findViewById<EditText>(R.id.editText_email)
+        val editText_password = findViewById<EditText>(R.id.editText_password)
+        val editText_confirm_password = findViewById<EditText>(R.id.editText_confirm_password)
+        val button_register = findViewById<Button>(R.id.button_register)
+        val textView_skip = findViewById<TextView>(R.id.textView_skip)
+        
+
+    }
+}
